7c385bfdbf6e79140173231e52157f7e7a89ad9b
Shanu <shanuflash@gmail.com>
2023-05-12 01:00:23 +0530

    test

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { createMiddlewareSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 export async function middleware(req) {
-  console.log("middleware2");
+  console.log("middleware1");
   const res = NextResponse.next();
   const pathname = req.nextUrl.pathname;
   const supabase = createMiddlewareSupabaseClient({ req, res });
@@ -10,14 +10,8 @@ export async function middleware(req) {
     data: { session },
   } = await supabase.auth.getSession();
   console.log(session);
-  if (!session && pathname === "/") {
-    const url = new URL(req.url);
-    url.pathname = "/login";
-    console.log("redirect1");
-    return NextResponse.redirect(url);
+  if (!session && pathname !== "/login") {
+    return NextResponse.redirect(new URL("/login", req.url));
   }
-  // if (!session && pathname !== "/login") {
-  //   return NextResponse.redirect("localhost:3000/login");
-  // }
   return res;
 }
