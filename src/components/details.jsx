264dc4d9e838812f36dedb5676462498c1249c10
Shanu Flash <shanuflash@gmail.com>
2023-05-11 15:45:12 +0530

    css modules for form

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,5 +1,7 @@
 "use client";
-import { React, useState } from "react";
+
+import { useState } from "react";
+import styles from "../styles/page.module.css";
 function App() {
   const [Data, setData] = useState({
     Date: "",
@@ -16,101 +18,96 @@ function App() {
     e.preventDefault();
   };
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit} className="forms">
-        <label>Date</label>
-        <input
-          type="date"
-          placeholder="Date"
-          value={Data.Date}
+    <form onSubmit={handleSubmit} className={styles.forms}>
+      <div className={styles["menu-title"]}>Add invoice details:</div>
+      <label>Date</label>
+      <input
+        type="date"
+        placeholder="Date"
+        value={Data.Date}
+        onChange={(e) => setData((prev) => ({ ...prev, Date: e.target.value }))}
+        required
+      />
+      <label>Invoice No:</label>
+      <input
+        type="text"
+        placeholder="Invoice No"
+        value={Data.InvoiceNo}
+        pattern="[0-9]+"
+        onChange={(e) =>
+          setData((prev) => ({ ...prev, InvoiceNo: e.target.value }))
+        }
+        required
+      />
+      <label>
+        Payment method:
+        <select
+          value={Data.Paymed}
           onChange={(e) =>
-            setData((prev) => ({ ...prev, Date: e.target.value }))
+            setData((prev) => ({ ...prev, Paymed: e.target.value }))
           }
-          required
-        />
-        <label>Invoice No:</label>
-        <input
-          type="text"
-          placeholder="Invoice No"
-          value={Data.InvoiceNo}
-          pattern="[0-9]+"
-          onChange={(e) =>
-            setData((prev) => ({ ...prev, InvoiceNo: e.target.value }))
-          }
-          required
-        />
-        <label>
-          Payment method:
-          <select
-            value={Data.Paymed}
-            onChange={(e) =>
-              setData((prev) => ({ ...prev, Paymed: e.target.value }))
-            }
-          >
-            <option value="">Select a payment method</option>
-            <option value="Cash">Cash</option>
-            <option value="Card">Card</option>
-            <option value="UPI">UPI</option>
-            <option value="Cheque">Cheque</option>
-          </select>
-        </label>
-        <label>Buyer's Name</label>
-        <input
-          type="text"
-          placeholder="Buyer's Name"
-          value={Data.Name}
-          pattern="^[a-zA-Z]+$"
-          onChange={(e) =>
-            setData((prev) => ({ ...prev, Name: e.target.value }))
-          }
-          required
-        />
-        <label>Buyer's Address</label>
-        <textarea
-          type="address"
-          placeholder="Buyer Address"
-          value={Data.Address}
-          onChange={(e) =>
-            setData((prev) => ({ ...prev, Address: e.target.value }))
-          }
-          required
-        />
-        <label>Buyer's Phone Number</label>
-        <input
-          type="text"
-          placeholder="Buyer Phone Number"
-          pattern="[0-9]+"
-          value={Data.PhoneNo}
-          onChange={(e) =>
-            setData((prev) => ({ ...prev, PhoneNo: e.target.value }))
-          }
-          required
-        />
-        <label>Deliver Name</label>
-        <input
-          type="text"
-          placeholder="Deliver Name"
-          value={Data.DelName}
-          pattern="^[a-zA-Z]+$"
-          onChange={(e) =>
-            setData((prev) => ({ ...prev, DelName: e.target.value }))
-          }
-          required
-        />
-        <label>Delivery Address</label>
-        <textarea
-          type="address"
-          placeholder="Delivery Address"
-          value={Data.DelAddress}
-          onChange={(e) =>
-            setData((prev) => ({ ...prev, DelAddress: e.target.value }))
-          }
-          required
-        />
+        >
+          <option value="">Select a payment method</option>
+          <option value="Cash">Cash</option>
+          <option value="Card">Card</option>
+          <option value="UPI">UPI</option>
+          <option value="Cheque">Cheque</option>
+        </select>
+      </label>
+      <label>Buyer's Name</label>
+      <input
+        type="text"
+        placeholder="Buyer's Name"
+        value={Data.Name}
+        pattern="^[a-zA-Z]+$"
+        onChange={(e) => setData((prev) => ({ ...prev, Name: e.target.value }))}
+        required
+      />
+      <label>Buyer's Address</label>
+      <textarea
+        type="address"
+        placeholder="Buyer Address"
+        value={Data.Address}
+        onChange={(e) =>
+          setData((prev) => ({ ...prev, Address: e.target.value }))
+        }
+        required
+      />
+      <label>Buyer's Phone Number</label>
+      <input
+        type="text"
+        placeholder="Buyer Phone Number"
+        pattern="[0-9]+"
+        value={Data.PhoneNo}
+        onChange={(e) =>
+          setData((prev) => ({ ...prev, PhoneNo: e.target.value }))
+        }
+        required
+      />
+      <label>Deliver Name</label>
+      <input
+        type="text"
+        placeholder="Deliver Name"
+        value={Data.DelName}
+        pattern="^[a-zA-Z]+$"
+        onChange={(e) =>
+          setData((prev) => ({ ...prev, DelName: e.target.value }))
+        }
+        required
+      />
+      <label>Delivery Address</label>
+      <textarea
+        type="address"
+        placeholder="Delivery Address"
+        value={Data.DelAddress}
+        onChange={(e) =>
+          setData((prev) => ({ ...prev, DelAddress: e.target.value }))
+        }
+        required
+      />
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      <button type="submit">Submit</button>
+    </form>
   );
 }
 
