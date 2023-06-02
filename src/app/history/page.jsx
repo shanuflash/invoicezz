62aabcc6794163c6503bbdad9a2dc6722e784a92
Shanu Flash <shanuflash@gmail.com>
2023-06-02 10:25:54 +0530

    add missing details in history

--- src/app/history/page.jsx
+++ src/app/history/page.jsx
@@ -9,6 +9,10 @@ const history = async () => {
     headers,
     cookies,
   });
+
+  // payref: "",
+  // disthro: "",
+
   const { data, error } = await supabase
     .from("history")
     .select("*")
@@ -23,14 +27,22 @@ const history = async () => {
         <div className={styles["history-item"]}>
           <div className={styles["item-invoiceno"]}>ID: {item.invoiceno}</div>
           <div className={styles["item-data"]}>Date: {item.date}</div>
-          <div className={styles["item-data"]}>Name: {item.name}</div>
-          <div className={styles["item-data"]}>GSTIN: {item.gstin}</div>
-          <div className={styles["item-data"]}>Address: {item.address}</div>
-          <div className={styles["item-data"]}>
-            Delivery Address: {item.delname}, {item.deladdress}
-          </div>
           <div className={styles["item-data"]}>
             Payment Method: {item.paymed}
+          </div>
+          <div className={styles["item-data"]}>GSTIN: {item.gstin}</div>
+          <div className={styles["item-data"]}>Payment Ref: {item.payref}</div>
+          <div className={styles["item-data"]}>Name: {item.name}</div>
+          <div className={styles["item-data"]}>
+            Delivery Name: {item.delname}
+          </div>
+          <div className={styles["item-data"]}>Address: {item.address}</div>
+          <div className={styles["item-data"]}>
+            Delivery Address: {item.deladdress}
+          </div>
+          <div className={styles["item-data"]}>Total: ₹{item.total}</div>
+          <div className={styles["item-data"]}>
+            Dispatched Through: {item.disthro}
           </div>
           <div className={styles["item-data"]}>
             Items:
@@ -43,7 +55,6 @@ const history = async () => {
                 );
             })}
           </div>
-          <div className={styles["item-data"]}>Total: ₹{item.total}</div>
         </div>
       ))}
     </div>
