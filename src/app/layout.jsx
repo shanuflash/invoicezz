8cc61d7ca66904ae16f95fa968533dd14183f9f4
Shanu Flash <shanuflash@gmail.com>
2023-06-11 15:25:06 +0530

    remove unwanted

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,8 +2,6 @@ import "../styles/globals.css";
 import styles from "../styles/page.module.css";
 import Nav from "../components/nav";
 
-
-
 import { Inter } from "next/font/google";
 import Providers from "@/redux/provider";
 
@@ -20,12 +18,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${inter.className} ${styles.body}`}>
-
-          <Providers>
-            <Nav />
-            <div className={styles.content}>{children}</div>
-          </Providers>
-
+        <Providers>
+          <Nav />
+          <div className={styles.content}>{children}</div>
+        </Providers>
       </body>
     </html>
   );
