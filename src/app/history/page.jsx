244665950be101d49a0d3134f1adda5bed94d519
Shanu Flash <shanuflash@gmail.com>
2023-06-20 13:54:09 +0530

    sort months in history

--- src/app/history/page.jsx
+++ src/app/history/page.jsx
@@ -1,26 +1,56 @@
+"use client";
 import styles from "@/styles/page.module.css";
 
-import { cookies } from "next/headers";
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-export const revalidate = 0;
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
 
-const history = async () => {
-  const supabase = createServerComponentClient({ cookies });
+const history = () => {
+  const [Data, setData] = useState([]);
+  const [month, setMonth] = useState(0);
+  const supabase = createClientComponentClient();
 
-  // payref: "",
-  // disthro: "",
+  const getHistory = async () => {
+    if (month === 0) {
+      const { data } = await supabase
+        .from("history")
+        .select("*")
+        .order("invoiceno", { ascending: false });
+      setData(data);
+      return;
+    }
+    const { data } = await supabase
+      .from("history")
+      .select("*")
+      .gte("date", `2023-0${month}-01`)
+      .lt("date", `2023-0${month + 1}-01`)
+      .order("invoiceno", { ascending: false });
+    return setData(data);
+  };
 
-  const { data, error } = await supabase
-    .from("history")
-    .select("*")
-    .order("invoiceno", { ascending: false });
+  useEffect(() => {
+    getHistory();
+  }, [month]);
+
+  const selectMonth = (e) => {
+    setMonth(e.target.value);
+  };
 
   return (
     <div className={styles["history"]}>
       <div className={styles["menu-title"]} style={{ padding: "0" }}>
         History
       </div>
-      {data.map((item) => (
+      <select
+        className={styles["history-select"]}
+        name="month"
+        id="month"
+        onChange={selectMonth}
+      >
+        <option value="0">All</option>
+        <option value="5">may</option>
+        <option value="6">june</option>
+      </select>
+      {Data?.map((item) => (
         <div className={styles["history-item"]}>
           <div className={styles["item-invoiceno"]}>ID: {item.invoiceno}</div>
           <div className={styles["item-data"]}>Date: {item.date}</div>
@@ -54,6 +84,12 @@ const history = async () => {
           </div>
         </div>
       ))}
+      <div className={styles["history-total"]}>
+        Total: ₹
+        {Data.reduce((acc, item) => {
+          return acc + item.total;
+        }, 0)}
+      </div>
     </div>
   );
 };
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,12 +17,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} ${styles.body}`}>
-        <Providers>
+      <Providers>
+        <body className={`${inter.className} ${styles.body}`}>
           <Nav />
           <div className={styles.content}>{children}</div>
-        </Providers>
-      </body>
+        </body>
+      </Providers>
     </html>
   );
 }
